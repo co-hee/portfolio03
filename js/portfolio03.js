9db4f13bca6113292be3786724eed5d20f451964
Seonghee Yun <104363065+intacthee@users.noreply.github.com>
2022-12-06 17:47:51 +0900

    Add files via upload

--- js/portfolio03.js
+++ js/portfolio03.js
@@ -1,14 +1,23 @@
 $(function () {
-    $('.mainVisual .mainSlide').slick();
+
+    $('.mainVisual .mainSlide').on('init afterChange', function () {
+        var current = $('.mainVisual .mainSlide .slick-current')
+        current.addClass('on').siblings().removeClass('on');
+    });
+
+
+    $('.mainVisual .mainSlide').slick({
+
+        autoplay: true,
+        autoplaySpeed: 4000,
+        pauseOnHOVER: false,
+        pauseOnFocus: false
+    });
 
 
 
 
 
+});
 
 
-
-
-
-
-})
