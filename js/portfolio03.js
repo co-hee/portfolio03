62f6a004395084702dd85817346621283f7a0a5b
Seonghee Yun <104363065+intacthee@users.noreply.github.com>
2022-12-09 16:57:28 +0900

    Add files via upload

--- js/portfolio03.js
+++ js/portfolio03.js
@@ -8,7 +8,7 @@ $(function () {
 
     $('.mainVisual .mainSlide').slick({
         dots: true,
-        autoplay: true,
+        //autoplay: true,
         autoplaySpeed: 4000,
         pauseOnHOVER: false,
         pauseOnFocus: false
